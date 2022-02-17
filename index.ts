d3d7f29790c8b75dd368c382186c846f2cd5be00
Vladislav Vavilikhin (contractor) <vladislav.vavilikhin@macys.com>
2022-02-17 14:10:36 +0300

    wip: save

--- index.ts
+++ index.ts
@@ -4,10 +4,27 @@ import * as prompts from 'prompts';
 
 const scenarios = await readdir(SCENARIOS_PATH);
 
+const questions: { scenario: string; questions: prompts.PromptObject[] }[] = [
+  //   {
+  //     type: 'text',
+  //     name: 'about',
+  //     message: 'Tell something about yourself',
+  //     initial: 'Why should I?',
+  //   },
+];
+
 for await (const scenario of scenarios) {
   const scenarioName = scenario.split('.')[0];
 
   const [main, inputParams] = await loadScenario(scenarioName);
+
+  // Создать функцию которпая по объекту будет создавать массив вопросов формата prompts.PromptObject[]
+  const questions;
+
+  questions.push({
+    scenario: scenarioName,
+    questions: [],
+  });
 
   console.log(inputParams);
 }
